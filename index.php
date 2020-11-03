cec6e8092b1d008733bab937a47450ccb9a1f172
rizaradiarivaldo <rizaradiarivaldo@gmail.com>
2020-11-03 14:35:47 +0700

    remake

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <title>
-
-        <?php session_start(); ?>
         <?php
-        if ($_SESSION['status'] == '') {
+        if ($_SESSION['status'] === '') {
             header('location:login.php?message=ditolak');
         };
         ?>
